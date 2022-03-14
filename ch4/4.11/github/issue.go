74305a343be51c0a3268f5c2a363708d6b21de0b
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2022-03-14 10:09:16 -0400

    started work on 4.11

---
+++ ch4/4.11/github/issue.go
@@ -0,0 +1,17 @@
+package github
+
+func get_issue(repo_url string, issue_id string) (*Issue, error) {
+	return nil, nil
+}
+
+func update_issue(repo_url string, issue *Issue) (*Issue, error) {
+	return nil, nil
+}
+
+func close_issue(repo_rul string, issue *Issue) (*Issue, error) {
+	return nil, nil
+}
+
+func create_issue(repo_url string) (*Issue, error) {
+	return nil, nil
+}
