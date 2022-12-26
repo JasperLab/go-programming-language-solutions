fcf38485952596be9e29e0bff8c782462cadda82
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2022-12-26 16:31:33 -0500

    re-organized solution structure with one directory per operation executable and a shared github library directory

---
+++ ch4/4.11/close/close.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"gopl/ch4/4.11/github"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args[1:]) != 3 {
+		printUsage()
+	}
+
+	owner, token, repo, issue_id := os.Args[1], os.Getenv("GITHUB_TOKEN"), os.Args[2], os.Args[3]
+	issue, err := github.GetIssue(owner, repo, issue_id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	issue.State = "closed"
+	issue, err = github.UpdateIssue(issue, owner, token, repo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("#%-5d %9.9s %.55s %.55s\n", issue.Number, issue.User.Login, issue.State, issue.Title)
+	fmt.Println("=====================")
+	fmt.Println(issue.Body)
+
+}
+
+func printUsage() {
+	fmt.Println("Usage:\n\tclose <owner> <repo> <id>")
+	os.Exit(1)
+}
---
+++ ch4/4.11/create/create.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"gopl/ch4/4.11/github"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args[1:]) != 3 {
+		printUsage()
+	}
+
+	issue, err := github.GetIssue(os.Args[1], os.Args[2], os.Args[3])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	issue.State = "closed"
+	issue, err = github.UpdateIssue(issue)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("#%-5d %9.9s %.55s\n", issue.Number, issue.User.Login, issue.Title)
+	fmt.Println("=====================")
+	fmt.Println(issue.Body)
+
+}
+
+func printUsage() {
+	fmt.Println("Usage:\n\t <owner> <repo> <description>")
+	os.Exit(1)
+}
