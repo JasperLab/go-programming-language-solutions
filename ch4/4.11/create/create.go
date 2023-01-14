d877b1189421e7857c321781dfba6291ceb18e3d
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2023-01-14 15:01:42 -0500

    implemented create issue

--- ch4/4.11/create/create.go
+++ ch4/4.11/create/create.go
@@ -2,30 +2,83 @@ package main
 
 import (
 	"fmt"
+	"gopl/ch4/4.11/github"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	if len(os.Args[1:]) < 1 {
+	if len(os.Args[1:]) < 3 {
 		printUsage()
 	}
+	args := os.Args[1:]
+	owner := args[0]
+	repo := args[1]
 
-	title := os.Args[1]
+	issue := &github.Issue{}
 
-	var assignees string[]
-	if len(os.Args[1:]) > 1 {
-		assignees = strings.Split(args[2])
+	issue.Title = args[2]
+	issue.User = &github.User{Login: owner}
+
+	// handle assignees - 4th argument
+	if len(args) > 3 {
+		split := strings.Split(os.Args[1:][1], ",")
+		for _, l := range split {
+			assignee := &github.Assignee{Login: l}
+			issue.Assignees = append(issue.Assignees, assignee)
+		}
 	}
 
-	var labels  string[]
-	if len(os.Args[1:] > 2) {
-		labels = strings.Split(args[3])
+	// handle labels - 5th argument
+	if len(args) > 4 {
+		split := strings.Split(os.Args[1:][2], ",")
+		for _, l := range split {
+			label := &github.Label{Name: l}
+			issue.Labels = append(issue.Labels, label)
+		}
 	}
 
-		
+	//handle issue body via vi
+	editor := "vi"
+	if s := os.Getenv("EDITOR"); s != "" {
+		editor = s
+	}
+	tmpDir := os.TempDir()
+	tmpFile, err := ioutil.TempFile(tmpDir, "tempFilePrefix")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tmpFile.Name())
+	cmd := exec.Command("sh", "-c", editor+" "+tmpFile.Name())
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := os.ReadFile(tmpFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	issue.Body = string(b)
+
+	issue, err = github.CreateIssue(owner, repo, issue)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("#%-5d %9.9s %.55s %.55s\n", issue.Number, issue.User.Login, issue.State, issue.Title)
+	fmt.Println("=====================")
+	fmt.Println(issue.Body)
 }
 
 func printUsage() {
-	fmt.Println("Usage:\n\t go create/create.go --title='title' [--assignee=name1[,name2,...] --labels[=label1[,label2,...]]]")
+	fmt.Println("Usage:\n\t go create/create.go <owner> <repo> <title> <login1[,login2,...]> <label1[,label2,...]>")
 	os.Exit(1)
 }
